5eb85dae39fdd3c1a2287d90bc7c5eeccbb33cdb
sunhao <sunhao.steven@bytedance.com>
2020-07-28 17:49:22 +0800

    feat: 简单工厂模式

--- createType.ts
+++ createType.ts
@@ -1,4 +1,4 @@
-// 单例模式：一个类只有一个事例，并提供一个访问它都全局访问点
+/* 单例模式：一个类只有一个事例，并提供一个访问它都全局访问点 */
 
 class SingletonOne {
   private constructor() {} // 构造器私有化，外部不可以new
@@ -10,7 +10,7 @@ class SingletonOne {
   }
 }
 
-console.log(SingletonOne.getInstance() === SingletonOne.getInstance())
+console.log(SingletonOne.getInstance() === SingletonOne.getInstance());
 
 class SingletonTwo {
   private constructor() {}
@@ -23,17 +23,42 @@ class SingletonTwo {
       return this.instance;
     } else return this.instance;
   }
-
 }
 
 console.log(SingletonTwo.getInstance() === SingletonTwo.getInstance());
 
+/* 简单工厂模式： 定义一个创建对象的类，用这个类封装实例化对象的行为 */
+
+// 抽象工厂类: 包含具体工厂公共的属性和方法
+
+interface Product {}
+
+// 具体工厂类：包含每个工厂特有的属性和方法 及公共的属性和方法
+class ProductOne implements Product {
+  constructor() {}
+}
+
+class ProductTwo implements Product {
+  constructor() {}
+}
+
 // 简单工厂
+class SimpleFactory {
+  public static createProduct(type: number) {
+    let product = null;
+    if (type === 1) {
+      product = new ProductOne();
+    } else if (type === 2) {
+      product = new ProductTwo();
+    }
+    return product;
+  }
+}
 
-// 工厂方法
+/* 工厂方法模式 */
 
-// 抽象工厂
+/* 抽象工厂模式 */
 
-// 原型
+/* 原型 */
 
-// 建造者
+/* 建造者 */
